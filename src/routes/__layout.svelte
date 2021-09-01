e63c66ac909bca8f46b164387b82822af435eb12
Alex Popoutsis <ajpopoutsis@gmail.com>
2021-09-01 05:30:20 +0000

    Add session js

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { session } from '$app/stores';
+    import { supabase } from '$lib/db';
+    import { setAuthCookie, unsetAuthCookie } from '$lib/utils/session';
+
+    // this should run on every page so I put this code in my `__layout.svelte` file
+    supabase.auth.onAuthStateChange(async (event, _session) => {
+    if (event !== 'SIGNED_OUT') {
+        session.set({ user: _session.user });
+        await setAuthCookie(_session);
+    } else {
+        session.set({ user: { guest: true } });
+        await unsetAuthCookie();
+    }
+    });
+</script>
