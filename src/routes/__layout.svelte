e2d8b7912e23e450d570efae862bdb11baef45a6
Alex Popoutsis <ajpopoutsis@gmail.com>
2021-09-01 06:34:05 +0000

    Refactor auth

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,23 +1,93 @@
 <script>
-    import { page, session } from '$app/stores';
-    import { supabase } from '$lib/db';
-    import { setAuthCookie, unsetAuthCookie } from '$lib/utils/session';
+    import { supabase } from '$lib/db'
+    import { page } from '$app/stores';
+    import { goto } from '$app/navigation';
+    const session = supabase.auth.session()
 
-    // this should run on every page so I put this code in my `__layout.svelte` file
-    supabase.auth.onAuthStateChange(async (event, _session) => {
-    if (event !== 'SIGNED_OUT') {
-        session.set({ user: _session.user });
-        await setAuthCookie(_session);
-    } else {
-        session.set({ user: { guest: true } });
-        await unsetAuthCookie();
+    let loading = false;
+    let email;
+    let password;
+
+    async function signOut() {
+        const { error } = await supabase.auth.signOut()
     }
-    });
+
+    async function signIn() {
+      loading = true;
+      let { error } = await supabase.auth.signIn({
+        email,
+        password
+      });
+      if (error) {
+        loading = false;
+        alert(error);
+        return;
+      }
+      let redirect = '/';
+      if (!password) {
+        redirect = `${redirect}?magic_link=true`;
+      }
+      goto(redirect);
+    }
 </script>
+  
 
 <main>
-    {#if $page.query.get('magic_link')}
-        <span style="text-align: center;">Check your email for login link!</span>
+    {#if session?.user?.id}<a href="#" on:click={signOut}>Sign Out</a><slot />
+    {:else if $page.query.get('magic_link')}
+    <span style="text-align: center;">Check your email for login link!</span>
+    {:else}
+    <div class="content">
+        <h1>Login</h1>
+      
+        <form on:submit|preventDefault={signIn}>
+          <div>
+            <input
+              id="email"
+              type="email"
+              autocomplete="email"
+              placeholder="Email"
+              required
+              bind:value={email}
+            />
+          </div>
+          <div>
+            <input
+              id="password"
+              type="password"
+              placeholder="Password (optional)"
+              bind:value={password}
+            />
+          </div>
+      
+          <div>
+            <button type="submit" class="submit" disabled={loading}>
+              {loading ? 'Loading ...' : 'Log In'}
+            </button>
+          </div>
+        </form>
+      </div>
+      
+
     {/if}
-    <slot />
+    
 </main>
+
+
+<style>
+    .content {
+      width: 100%;
+      max-width: var(--column-width);
+      margin: var(--column-margin-top) auto 0 auto;
+    }
+    .submit {
+      border-radius: 20px;
+      font-size: 12px;
+      font-weight: bold;
+      padding: 12px 45px;
+      letter-spacing: 1px;
+      text-transform: uppercase;
+      transition: transform 80ms ease-in;
+      cursor: pointer;
+    }
+  </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,4 @@
-<script context="module">
-  // see https://kit.svelte.dev/docs#loading
-  export const load = async ({ fetch, session }) => {
-    // redirect to login if user is not logged in
-    if (!session?.user?.id) {
-      return {
-        status: 301,
-        redirect: '/login'
-      };
-    }
-  };
-</script>
+
 
 
 
