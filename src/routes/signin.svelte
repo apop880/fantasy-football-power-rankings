1af397a1be322372eb535d7a4ac7053eaaf93c83
Alex Popoutsis <ajpopoutsis@gmail.com>
2021-09-02 22:11:53 +0000

    Refactor signin layout to use Tailwind

--- src/routes/signin.svelte
+++ src/routes/signin.svelte
@@ -17,43 +17,25 @@
     }
 </script>
 
-<div class="content">
-        <h1>Enter Your Email to Receive Login Link</h1>
-      
-        <form on:submit|preventDefault={signIn}>
-          <div>
-            <input
+<div class="flex items-center justify-center h-screen">
+  <div class="bg-gray-800 flex flex-col w-80 border border-gray-900 rounded-lg px-8 py-10">
+  <div class="text-white mt-10">
+    <h1 class="font-bold text-4xl">Sign In</h1>
+    <p class="font-semibold">Enter Your Email to Receive a Login Link</p>
+  </div>
+  <form class="flex flex-col space-y-8 mt-10" on:submit|preventDefault={signIn}>
+    <input
               id="email"
               type="email"
               autocomplete="email"
               placeholder="Email"
               required
               bind:value={email}
+              class="border rounded-lg py-3 px-3 bg-gray-700 border-gray-700 placeholder-gray-500"
             />
-          </div>
-      
-          <div>
-            <button type="submit" class="submit" disabled={magic}>
-              {magic ? 'Check Email for Login Link' : 'Log In'}
-            </button>
-          </div>
-        </form>
-      </div>
-
-<style>
-    .content {
-      width: 100%;
-      max-width: var(--column-width);
-      margin: var(--column-margin-top) auto 0 auto;
-    }
-    .submit {
-      border-radius: 20px;
-      font-size: 12px;
-      font-weight: bold;
-      padding: 12px 45px;
-      letter-spacing: 1px;
-      text-transform: uppercase;
-      transition: transform 80ms ease-in;
-      cursor: pointer;
-    }
-  </style>
+    <button type="submit" disabled={magic} class="border border-blue-500 bg-blue-500 text-white rounded-lg py-3 font-semibold">
+      {magic ? 'Check Email for Login Link' : 'Log In'}
+    </button>
+  </form>
+</div>
+</div>
