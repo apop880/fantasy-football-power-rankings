7a0d382578f356653fc896a54f52422b081ac1eb
Alex Popoutsis <ajpopoutsis@gmail.com>
2021-09-05 22:36:55 +0000

    Add form with binding

--- src/routes/editor.svelte
+++ src/routes/editor.svelte
@@ -19,6 +19,8 @@
 
     let formData = [];
     let loading = false;
+
+    $: console.log(data)
     
     async function submitData() {
       loading = true
@@ -35,13 +37,13 @@
 </script>
 
 <section class="max-w-xl mx-auto p-4 flex flex-col gap-1">
-{#each data as row}
+{#each data as row, i}
 <div class="grid grid-cols-7 bg-gray-100 border-2 border-gray-600 rounded-md px-2">
   <div class="row-span-2 self-center"><strong>{row.ranking}</strong></div>
   <div class="col-span-5"><strong>{row.name}</strong> ({row.pts} pts{#if row.first_place_votes > 0}, {row.first_place_votes} first place vote{#if row.first_place_votes > 1}s{/if}{/if})</div>
   <div class="row-span-2 self-center">{#if row.week !== 0}<strong>{row.prev_ranking}</strong>{/if}</div>
   <div class="col-span-5">{row.wins}-{row.losses} {#if row.week === 0} Last Season{/if}, {row.place} in {row.division}</div>
-  <div class="col-span-7 border-t border-gray-600">Form area</div>
+  <div class="col-span-7 border-t border-gray-600"><textarea placeholder="Team Description" class="border rounded-md w-full p-2" bind:value="{row.description}"></textarea></div>
 </div>
 {/each}
 </section>
