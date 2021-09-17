259394e12118554ca133eb6c6d77b23fe3ea84c5
Alex Popoutsis <41379678+apop880@users.noreply.github.com>
2021-09-17 14:10:04 -0500

    Remove extra space

--- src/lib/Results.svelte
+++ src/lib/Results.svelte
@@ -22,7 +22,7 @@
   <div class="row-span-2 self-center text-xl"><strong>{row.ranking}</strong></div>
   <div class="col-span-5"><strong>{row.name}</strong> ({row.pts} pts{#if row.first_place_votes > 0}, {row.first_place_votes} first place vote{#if row.first_place_votes > 1}s{/if}{/if})</div>
   <div class="row-span-2 self-center">{#if row.week !== 0}<strong>{row.prev_ranking}</strong>{/if}</div>
-  <div class="col-span-5">{row.wins}-{row.losses} {#if row.week === 0} Last Season{/if}, {row.place} in {row.division}</div>
+  <div class="col-span-5">{row.wins}-{row.losses}{#if row.week === 0} Last Season{/if}, {row.place} in {row.division}</div>
   <div class="col-span-7 border-t border-gray-600 pt-1 pb-1 mt-1">{row.description}</div>
 </div>
 {/each}
